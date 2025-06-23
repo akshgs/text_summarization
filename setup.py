bf920e2ee18c88964a285898c1d19103d877910a
akshgs <akashgs369@gmail.com>
2025-06-23 11:13:21 +0530

    end to end text summarization

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "text_summarization"
+AUTHOR_USER_NAME = "akshgs"
+SRC_REPO = "text_summarization"
+AUTHOR_EMAIL = "akashgs369@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Python library for summarizing text.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
